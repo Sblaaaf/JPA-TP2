c47d7a46bf8c2f266901b413bcd038e44bbfd9c4
RENO <reno@MacBook-Air-de-RENO.local>
2026-02-06 10:44:57 +0100

    MAJ de client avec lien vers emprunt et guetters setters

--- src/main/java/fr/epsi_26/Client.java
+++ src/main/java/fr/epsi_26/Client.java
@@ -2,6 +2,8 @@ package fr.epsi_26;
 
 import jakarta.persistence.*;
 import java.io.Serializable;
+import java.util.Set;
+import java.util.HashSet;
 
 @Entity
 @Table(name = "CLIENT")
@@ -18,36 +20,34 @@ public class Client implements Serializable {
     @Column(name = "PRENOM", nullable = false, length = 50)
     private String prenom;
 
+    // Lien vers les emprunts du client
+    @OneToMany(mappedBy = "client")
+    private Set<Emprunt> emprunts;
+
     public Client() {
+        this.emprunts = new HashSet<>();
     }
 
     public Client(String nom, String prenom) {
         this.nom = nom;
         this.prenom = prenom;
+        this.emprunts = new HashSet<>();
     }
 
-    public Integer getId() {
-        return id;
+    public Integer getId() { return id; }
+    public void setId(Integer id) { this.id = id; }
+    public String getNom() { return nom; }
+    public void setNom(String nom) { this.nom = nom; }
+    public String getPrenom() { return prenom; }
+    public void setPrenom(String prenom) { this.prenom = prenom; }
+
+    // Getter et Setter pour emprunts
+    public Set<Emprunt> getEmprunts() {
+        return emprunts;
     }
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getNom() {
-        return nom;
-    }
-
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
-
-    public String getPrenom() {
-        return prenom;
-    }
-
-    public void setPrenom(String prenom) {
-        this.prenom = prenom;
+    public void setEmprunts(Set<Emprunt> emprunts) {
+        this.emprunts = emprunts;
     }
 
     @Override
