46b578ecfd0eaf348ce03dbac082be318cab2434
RENO <reno@MacBook-Air-de-RENO.local>
2026-02-06 09:27:29 +0100

    update - modification livre 5

--- src/main/java/fr/epsi_26/App.java
+++ src/main/java/fr/epsi_26/App.java
@@ -20,6 +20,16 @@ public class App {
             System.out.println(">>> Livre 1 trouvé : " + livre1.getTitre() + " de " + livre1.getAuteur());
         }
 
+        // UPDATE
+        // Modifier le titre du livre 5
+        Livre livre5 = em.find(Livre.class, 5);
+        if (livre5 != null) {
+            livre5.setTitre("Du plaisir dans la cuisine");
+            System.out.println(">>> Livre 5 modifié avec succès.");
+        } else {
+            System.out.println(">>> Livre 5 introuvable !");
+        }
+
         em.getTransaction().commit();
         em.close();
         emf.close();
