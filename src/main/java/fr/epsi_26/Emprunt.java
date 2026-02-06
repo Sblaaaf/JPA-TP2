06f31ba4948aff55576165f563bcdbea8c12a947
RENO <reno@MacBook-Air-de-RENO.local>
2026-02-06 10:32:42 +0100

    creation de la table emprunt

---
+++ src/main/java/fr/epsi_26/Emprunt.java
@@ -0,0 +1,104 @@
+package fr.epsi_26;
+
+import jakarta.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "EMPRUNT")
+public class Emprunt implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Integer id;
+
+    @Column(name = "DATE_DEBUT", nullable = false)
+    private LocalDateTime dateDebut;
+
+    @Column(name = "DATE_FIN")
+    private LocalDateTime dateFin;
+
+    @Column(name = "DELAI")
+    private Integer delai;
+
+    // Relation ManyToOne : Un emprunt concerne un client
+    @ManyToOne
+    @JoinColumn(name = "ID_CLIENT", nullable = false)
+    private Client client;
+
+    // Relation ManyToMany : Un emprunt contient plusieurs livres
+    @ManyToMany
+    @JoinTable(name = "COMPO",
+            joinColumns = @JoinColumn(name = "ID_EMP"),
+            inverseJoinColumns = @JoinColumn(name = "ID_LIV")
+    )
+    private Set<Livre> livres;
+
+    // Constructeur vide
+    public Emprunt() {
+        this.livres = new HashSet<>();
+    }
+
+    // Getters et Setters
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getDateDebut() {
+        return dateDebut;
+    }
+
+    public void setDateDebut(LocalDateTime dateDebut) {
+        this.dateDebut = dateDebut;
+    }
+
+    public LocalDateTime getDateFin() {
+        return dateFin;
+    }
+
+    public void setDateFin(LocalDateTime dateFin) {
+        this.dateFin = dateFin;
+    }
+
+    public Integer getDelai() {
+        return delai;
+    }
+
+    public void setDelai(Integer delai) {
+        this.delai = delai;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public Set<Livre> getLivres() {
+        return livres;
+    }
+
+    public void setLivres(Set<Livre> livres) {
+        this.livres = livres;
+    }
+
+    @Override
+    public String toString() {
+        // Attention : ne pas afficher 'client' ou 'livres' ici pour éviter les boucles infinies
+        return "Emprunt{" +
+                "id=" + id +
+                ", dateDebut=" + dateDebut +
+                ", dateFin=" + dateFin +
+                ", delai=" + delai +
+                '}';
+    }
+}
