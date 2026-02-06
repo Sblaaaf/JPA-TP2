d73ef46efaf9e1935857943871604dc5d2ec934c
RENO <reno@MacBook-Air-de-RENO.local>
2026-02-06 10:29:33 +0100

    création de client

---
+++ src/main/java/fr/epsi_26/Client.java
@@ -0,0 +1,61 @@
+package fr.epsi_26;
+
+import jakarta.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "CLIENT")
+public class Client implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Integer id;
+
+    @Column(name = "NOM", nullable = false, length = 50)
+    private String nom;
+
+    @Column(name = "PRENOM", nullable = false, length = 50)
+    private String prenom;
+
+    public Client() {
+    }
+
+    public Client(String nom, String prenom) {
+        this.nom = nom;
+        this.prenom = prenom;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", nom='" + nom + '\'' +
+                ", prenom='" + prenom + '\'' +
+                '}';
+    }
+}
