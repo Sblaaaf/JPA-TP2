54b23f0bf87c0249309c2dbe95fe86570f695da5
RENO <reno@MacBook-Air-de-RENO.local>
2026-02-06 09:23:47 +0100

    find titre - auteur

--- src/main/java/fr/epsi_26/App.java
+++ src/main/java/fr/epsi_26/App.java
@@ -13,6 +13,13 @@ public class App {
         Livre nouveauLivre = new Livre("Harry Potter à l'école des sorciers", "J.K. Rowling");
         em.persist(nouveauLivre);
         System.out.println(">>> Nouveau livre inséré avec l'ID : " + nouveauLivre.getId());
+
+        // FIND
+        Livre livre1 = em.find(Livre.class, 1);
+        if (livre1 != null) {
+            System.out.println(">>> Livre 1 trouvé : " + livre1.getTitre() + " de " + livre1.getAuteur());
+        }
+
         em.getTransaction().commit();
         em.close();
         emf.close();
