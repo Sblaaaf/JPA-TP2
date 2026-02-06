2e978597f8ed541944d29a561d3d87da58898b83
RENO <reno@MacBook-Air-de-RENO.local>
2026-02-06 10:46:22 +0100

    MAJ de Livre - emprunt

--- src/main/java/fr/epsi_26/Livre.java
+++ src/main/java/fr/epsi_26/Livre.java
@@ -2,6 +2,8 @@ package fr.epsi_26;
 
 import jakarta.persistence.*;
 import java.io.Serializable;
+import java.util.Set;
+import java.util.HashSet;
 
 @Entity
 @Table(name = "LIVRE")
@@ -18,39 +20,34 @@ public class Livre implements Serializable {
     @Column(name = "AUTEUR", nullable = false, length = 50)
     private String auteur;
 
-    // Constructeur vide (Obligatoire pour JPA)
+    // Lien vers les emprunts - livre
+    @ManyToMany(mappedBy = "livres")
+    private Set<Emprunt> emprunts;
+
     public Livre() {
+        this.emprunts = new HashSet<>();
     }
 
-    // Constructeur avec paramètres
     public Livre(String titre, String auteur) {
         this.titre = titre;
         this.auteur = auteur;
+        this.emprunts = new HashSet<>();
     }
 
-    // Getters et Setters
-    public Integer getId() {
-        return id;
+    public Integer getId() { return id; }
+    public void setId(Integer id) { this.id = id; }
+    public String getTitre() { return titre; }
+    public void setTitre(String titre) { this.titre = titre; }
+    public String getAuteur() { return auteur; }
+    public void setAuteur(String auteur) { this.auteur = auteur; }
+
+    // Getter et Setter pour emprunts
+    public Set<Emprunt> getEmprunts() {
+        return emprunts;
     }
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getTitre() {
-        return titre;
-    }
-
-    public void setTitre(String titre) {
-        this.titre = titre;
-    }
-
-    public String getAuteur() {
-        return auteur;
-    }
-
-    public void setAuteur(String auteur) {
-        this.auteur = auteur;
+    public void setEmprunts(Set<Emprunt> emprunts) {
+        this.emprunts = emprunts;
     }
 
     @Override
